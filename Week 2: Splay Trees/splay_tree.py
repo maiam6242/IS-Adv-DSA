4961164a73a8202abeefaa7e3e452b88921cd76b
Maia Materman <maiam6242@gmail.com>
2022-03-09 09:01:42 -0500

    :sun:

--- Week 2: Splay Trees/splay_tree.py
+++ Week 2: Splay Trees/splay_tree.py
@@ -6,9 +6,6 @@ More Information: https://rpucella.net/other/is-dsa-sp22/
 
 import sys
 
-
-SENTINEL = -99
-
 class Node():
     def __init__(self, key, val):
         ''' 
@@ -19,14 +16,81 @@ class Node():
         self.right = None
         self.left = None
         self.parent = None
+    
+    def setLeft(self, n):
+        '''
+        This method sets the left child of a node to a given node, n
+        '''
+        self.left = n
+        if n:
+            n.parent = self
+    
+    def setRight(self, n):
+        '''
+        This method sets the right child of a node to a given node, n
+        '''
+        self.right = n
+        if n:
+            n.parent = self
+    
+    def setParent(self, n):
+        '''
+        This method sets the parent of a node to a given node, n
+        '''
+        self.parent = n
+    
+    def isLeftChild(self):
+        '''
+        This method returns true if n is a left child, and false otherwise
+        '''
+        return self.parent.left == self
+
+    def isRightChild(self):
+        '''
+        This method returns true if n is a right child, and false otherwise
+        '''
+        return self.parent.right == self
 
 class SplayTree():
     def __init__(self):
         '''
         This method initializes the Splay Tree to be blank
         '''
-        self.SENTINEL = Node(SENTINEL, SENTINEL)
-        self.root = self.SENTINEL
+        self.root = None
+
+    def get_maximum(self, n):
+        '''
+        This method returns the maximum of a tree, given a starting node
+        '''
+        
+        while n.right is not None:
+            n = n.right
+        return n
+    
+    def join(self, left, right):
+        '''
+        This method joins to subtrees from their root nodes
+        '''
+        # print('left node value at join ' + str(left.value))
+        print('right node value at join ' + str(right.value))
+        if not right:
+            return left
+        if not left:
+            return right
+
+        # find maximum
+        max = left.get_maximum(left.root)
+        print('max of tree w/o deletion ' + str(max.value))
+       
+        if left.root != max:
+            left.splay(max)
+        # make the max the new root of the tree
+        left.print_tree()
+        print('splayed in join')
+        max.setRight(right) 
+        # will this always work?
+        max.setLeft(left.root.left)
+        return max
 
     def right_rotate(self, starting_top_node):
         '''
@@ -35,131 +99,108 @@ class SplayTree():
         starting_left_child = starting_top_node.left
        
         # take the right subtree of the starting left child and make it the left subtree of the top node
-        starting_top_node.left = starting_left_child.right
+        starting_top_node.setLeft(starting_left_child.right) 
 
-        if starting_left_child.right and starting_left_child.right != self.SENTINEL:
+        if starting_left_child.right:
             starting_left_child.right.parent = starting_top_node
 
         # the parent of the starting top node becomes the parent of the starting left node
-        starting_left_child.parent = starting_top_node.parent
+        starting_left_child.setParent(starting_top_node.parent)
  
         # make the starting left child the of the tree if the starting top node was a child of the root
         if starting_top_node.parent == None:
             self.root = starting_left_child
 
         # retain the binary tree properties, arrange children accordingly(?)
-        elif starting_top_node == starting_top_node.parent.right:
+        elif starting_top_node.isRightChild():
             starting_top_node.parent.right = starting_left_child 
 
         else:
             starting_top_node.parent.left = starting_left_child 
 
         # carry over the rest of the tree, put the starting top node as the right child of the starting left node (which is now on top!)
-        starting_left_child.right = starting_top_node 
-        starting_top_node.parent = starting_left_child   
+        starting_left_child.setRight(starting_top_node) 
+        starting_top_node.setParent(starting_left_child)   
 
     def left_rotate(self, starting_top_node):
         '''
         This method is a left rotation, helping to allow the preservation of the red-black tree structure during an insertion 
         '''
         print(starting_top_node.value)
-        
         starting_right_child = starting_top_node.right
         
-        print(starting_right_child.value)
-
         # take the left subtree of the starting right child and make it the right subtree of the top node
-        starting_top_node.right = starting_right_child.left
+        starting_top_node.setRight(starting_right_child.left)
 
-        if starting_right_child.left != None:
-         
+        if starting_right_child.left:
             starting_right_child.left.parent = starting_top_node
 
         # the parent of the starting top node becomes the parent of the starting right node
-        starting_right_child.parent = starting_top_node.parent
+        starting_right_child.setParent(starting_top_node.parent)
 
         # make the starting left child the of the tree if the starting top node was a child of the root
         if starting_top_node.parent == None:
             self.root = starting_right_child
 
         # retain the binary tree properties, arrange children accordingly(?)
-        elif starting_top_node == starting_top_node.parent.left:
+        elif starting_top_node.isLeftChild():
             starting_top_node.parent.left = starting_right_child 
 
         else:
             starting_top_node.parent.right = starting_right_child 
 
         # carry over the rest of the tree, put the starting top node as the left child of the starting right node (which is now on top!)
-        starting_right_child.left = starting_top_node 
-        starting_top_node.parent = starting_right_child   
-        print("left rotation")
-        self.print_tree()  
+        starting_right_child.setLeft(starting_top_node) 
+        starting_top_node.setParent(starting_right_child)    
 
     def splay(self, node_for_top):
         '''
+        This method splays the tree to place a node, node_for_top on the top of the tree
         '''
+
         # while the node is not root
-        while node_for_top.parent and node_for_top.parent.value is not SENTINEL:
+        while node_for_top.parent:
             print("splaying " + str(node_for_top.value))
             # if the child of the root, so only needing one rotation
-            if node_for_top.parent.parent is None or node_for_top.parent.parent.value is SENTINEL:
+            print(type(node_for_top.parent.parent))
+            if node_for_top.parent.parent is None:
                 # make either right or left rotation to get the node for top on the top of the tree
-                if node_for_top == node_for_top.parent.left:
-                    print("hellloooo")
+                if node_for_top.isLeftChild():
                     self.right_rotate(node_for_top.parent)
                 else:
-                    print("heyyy")
                     self.left_rotate(node_for_top.parent) 
             # need two rotations
-            # TODO: Finish commenting
             else:
-                n_parent = node_for_top.parent
-                n_gparent = n_parent.parent
+                # there are four possible double rotations, if the parent and grandparent are in a line on the left, if they are in a line on the right, or if they are on opposite sides, either with the parent being on right or left of grandparent. All of these warrant different rotations which are the cases below
 
-                # if node_for_top.parent.left == node_for_top and node_for_top.parent.parent.left == node_for_top.parent:
-                if node_for_top.parent.left == node_for_top and n_parent.parent.left == n_parent:
-                    print("this")
-                    self.right_rotate(n_gparent)
-                    self.right_rotate(n_parent)
-                elif node_for_top.parent.right == node_for_top and n_parent.parent.right == n_parent:
-                    print("that")
-                    self.left_rotate(n_gparent)
-                    self.left_rotate(n_parent)
-                elif node_for_top.parent.right == node_for_top and n_parent.parent.left == n_parent:
-                    print("zig")
-                    print(node_for_top)
-                    print(node_for_top.parent.parent.value)
-                    self.left_rotate(n_parent)
-                    print(node_for_top)
-                    print(node_for_top.parent.parent.value)
-                    self.right_rotate(n_gparent)
-                elif node_for_top.parent.left == node_for_top and n_parent.parent.right == n_parent:
-                    print("zag")
-                    print(node_for_top)
-                    print(node_for_top.parent.value)
-                    self.right_rotate(n_parent)
-                    self.left_rotate(n_gparent)
-
-            self.print_tree()
+                if node_for_top.isLeftChild() and node_for_top.parent.isLeftChild():
+                    self.right_rotate(node_for_top.parent.parent)
+                    self.right_rotate(node_for_top.parent)
+                elif node_for_top.isRightChild() and node_for_top.parent.isRightChild():
+                    self.left_rotate(node_for_top.parent.parent)
+                    self.left_rotate(node_for_top.parent)
+                elif node_for_top.isRightChild() and node_for_top.parent.isLeftChild():
+                    self.left_rotate(node_for_top.parent)
+                    if node_for_top.parent.parent:
+                        self.right_rotate(node_for_top.parent.parent)
+                elif node_for_top.isLeftChild() and node_for_top.parent.isRightChild():
+                    self.right_rotate(node_for_top.parent)
+                    if node_for_top.parent.parent:
+                        self.left_rotate(node_for_top.parent.parent)
+       
+        self.print_tree()
 
     def search(self, k):
         '''
         This method returns the value stored in the node at key k, or None if no such key is found
         '''
-        current_node = self.root
-
-        while(current_node and current_node.key != SENTINEL and current_node.key != k):
-            if k < current_node.key:
-                current_node = current_node.left
-            else:
-                current_node = current_node.right
-            
-        if current_node is None:
-            return None 
-
-        if current_node.key == k:
-            self.splay(current_node)
-            return current_node.value 
+        n = self.search_return_node(k)
+        if not n:
+            return None
+        else:
+            print(n.value)
+            self.splay(n)
+            return n.value 
     
     def search_return_node(self,k):
         '''
@@ -203,32 +244,60 @@ class SplayTree():
             
             node_to_insert.parent = y
 
-            # check if the tree is empty, if it is, insert the new node as the root and make it black
-            if y == self.SENTINEL:
+            # check if the tree is empty, if it is, insert the new node as the root
+            if y == None:
                 self.root = node_to_insert
-                print('get here')
-                print(y.value)
                 return
            
             # if not, place it in the tree where it should be (according to binary search tree rules)
             elif node_to_insert.key < y.key:
-                print("should be here?")
                 y.left = node_to_insert
 
             else:
-                print("or here?")
                 y.right = node_to_insert
-            
-        # print(self.print_tree())
+
+        # splay the tree to place newly inserted node on top
         self.splay(node_to_insert)
 
     def delete(self, k):
         '''
         This method deletes the node at key k if it exists
         '''
+        # find the node to be deleted
+        node_to_delete = self.search_return_node(k)
+        # if key isn't in tree, return 
+        if not node_to_delete:
+            return
+        else:
+            # splay the tree, deletion of the root node gives us two subtrees 
+            self.splay(node_to_delete)
+
+            # create the right subtree
+            if self.root.right:
+                right_subtree = self.root.right 
+                right_subtree.setParent(None)
+            else:
+                right_subtree = None
+
+            # create the left subtree
+            
+            # right_subtree = self.root.right 
+
+            if self.root.left:
+                left_subtree = SplayTree()
+                left_subtree.root = self.root.left
+                # left_subtree.setParent(None)
+            else:
+                left_subtree = None
+
+            # join the two subtrees 
+            # why doesn't this tree persist??           
+            self.root = self.join(left_subtree, right_subtree)
+            print("line 295")
+            self.print_tree()
 
     def __print_helper(self, node, indent, last):
-        if node and node != self.SENTINEL:
+        if node:
             sys.stdout.write(indent)
             if last:
                 sys.stdout.write("R----")
@@ -244,21 +313,21 @@ class SplayTree():
     def print_tree(self):
         self.__print_helper(self.root, "", True)
 
-
 if __name__ == "__main__":
     tree1 = SplayTree()
     # fill a tree with integers
     for x in range(1,7):
        tree1.insert(x, x*20)
-    
     tree1.print_tree()
+    print(tree1.search(2))
     print(tree1.search(3))
-    # # nprint(tree1.search(25))
-    # tree1.delete(10)
-    # tree1.print_tree()
+    print(tree1.search(6))
+    # print(tree1.search(25))
+    tree1.delete(4)
+    tree1.print_tree()
     # # tree1.print_tree()
-    # tree1.delete(4)
-    # tree1.print_tree()
+    tree1.delete(2)
+    tree1.print_tree()
     # tree1.delete(20)
     # tree1.print_tree()
     # tree1.delete(27)
